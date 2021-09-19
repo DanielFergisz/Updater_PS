0de5d877a7ff16a81c209219dad906a46fe31377
Daniel Fergisz <daniel.fergisz@gmail.com>
2021-09-19 22:57:52 +0200

    v1.03 - changed "sleep" to timer and added text

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -26,15 +26,20 @@ Partial Class Form1
         Me.Log = New System.Windows.Forms.RichTextBox()
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
         Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer4 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer5 = New System.Windows.Forms.Timer(Me.components)
         Me.SuspendLayout()
         '
         'Log
         '
+        Me.Log.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Log.Font = New System.Drawing.Font("Verdana", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
         Me.Log.Location = New System.Drawing.Point(9, 10)
         Me.Log.Margin = New System.Windows.Forms.Padding(2)
         Me.Log.Name = "Log"
         Me.Log.ReadOnly = True
-        Me.Log.Size = New System.Drawing.Size(334, 84)
+        Me.Log.Size = New System.Drawing.Size(334, 111)
         Me.Log.TabIndex = 0
         Me.Log.Text = ""
         '
@@ -46,16 +51,30 @@ Partial Class Form1
         '
         Me.Timer2.Interval = 1000
         '
+        'Timer3
+        '
+        Me.Timer3.Interval = 400
+        '
+        'Timer4
+        '
+        Me.Timer4.Interval = 3000
+        '
+        'Timer5
+        '
+        Me.Timer5.Interval = 1000
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(354, 102)
+        Me.ClientSize = New System.Drawing.Size(354, 130)
         Me.Controls.Add(Me.Log)
         Me.Margin = New System.Windows.Forms.Padding(2)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "UPDATER PlayStation OS v1.02"
+        Me.Text = "PS_OS Updater v1.03"
         Me.ResumeLayout(False)
 
     End Sub
@@ -63,4 +82,7 @@ Partial Class Form1
     Friend WithEvents Log As RichTextBox
     Friend WithEvents Timer1 As Timer
     Friend WithEvents Timer2 As Timer
+    Friend WithEvents Timer3 As Timer
+    Friend WithEvents Timer4 As Timer
+    Friend WithEvents Timer5 As Timer
 End Class
--- Form1.vb
+++ Form1.vb
@@ -12,59 +12,58 @@ Public Class Form1
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
         Timer1.Enabled = False
-        Log.SelectionColor = Color.ForestGreen
-        Log.AppendText("OK")
-        Log.SelectionColor = Color.Empty
-        If File.Exists("PS_OS.exe") Then
-            Log.AppendText(Environment.NewLine + "")
-            If File.Exists("newFirmware.fwx") Then
-                Log.AppendText(Environment.NewLine + "Delete old firmware files.. ")
 
-                If File.Exists("PS5\FULL\PS5UPDATE.PUP") Then
-                    File.Delete("PS5\FULL\PS5UPDATE.PUP")
-                End If
-                If File.Exists("PS5\UPDATE\PS5UPDATE.PUP") Then
-                    File.Delete("PS5\UPDATE\PS5UPDATE.PUP")
-                End If
-                If File.Exists("PS4\FULL\PS4UPDATE.PUP") Then
-                    File.Delete("PS4\FULL\PS4UPDATE.PUP")
-                End If
-                If File.Exists("PS4\UPDATE\PS4UPDATE.PUP") Then
-                    File.Delete("PS4\UPDATE\PS4UPDATE.PUP")
-                End If
-                If File.Exists("PS3\PS3UPDAT.PUP") Then
-                    File.Delete("PS3\PS3UPDAT.PUP")
-                End If
-                If File.Exists("PSV\PSVUPDAT.PUP") Then
-                    File.Delete("PSV\PSVUPDAT.PUP")
-                End If
-                If File.Exists("PSP\EBOOT.PBP") Then
-                    File.Delete("PSP\EBOOT.PBP")
-                End If
-                Log.SelectionColor = Color.ForestGreen
-                Log.AppendText("OK")
-                Log.SelectionColor = Color.Empty
-            End If
-            Log.AppendText(Environment.NewLine + "Rename old version..")
+        If File.Exists("PS_OS.exe") Then
+            Log.SelectionColor = Color.ForestGreen
+            Log.AppendText("OK")
+            Log.SelectionColor = Color.Empty
+            Log.AppendText(Environment.NewLine + "")
+            Log.AppendText(Environment.NewLine + "Rename old version.. ")
             Timer2.Enabled = True
         Else
+            Log.SelectionColor = Color.Red
+            Log.AppendText("Failure !!!")
+            Log.SelectionColor = Color.Empty
             Log.AppendText(Environment.NewLine + "")
             Log.AppendText(Environment.NewLine + "No application to update.")
-
         End If
     End Sub
 
     Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
         Timer2.Enabled = False
         My.Computer.FileSystem.RenameFile("PS_OS.exe", "PS_OS.exe.old")
+        Log.SelectionColor = Color.ForestGreen
+        Log.AppendText("OK")
+        Log.SelectionColor = Color.Empty
+        Log.AppendText(Environment.NewLine + "Download new version.. ")
+        Timer3.Enabled = True
+    End Sub
 
-        Log.AppendText(Environment.NewLine + "Download new version...")
-        Threading.Thread.Sleep(300)
+    Private Sub Timer3_Tick(sender As Object, e As EventArgs) Handles Timer3.Tick
+        Timer3.Enabled = False
         client.DownloadFile("http://repairbox.pl/PS_OS/" + newVersion + "/PS_OS.exe", appPath + "\PS_OS.exe")
         client.Dispose()
-        Threading.Thread.Sleep(3000)
+        Log.SelectionColor = Color.ForestGreen
+        Log.AppendText("OK")
+        Log.SelectionColor = Color.Empty
+        Timer4.Enabled = True
+        Log.AppendText(Environment.NewLine + "Updating.. ")
+    End Sub
+
+    Private Sub Timer4_Tick(sender As Object, e As EventArgs) Handles Timer4.Tick
+        Timer4.Enabled = False
+        Log.SelectionColor = Color.ForestGreen
+        Log.AppendText("OK")
+        Log.SelectionColor = Color.Empty
+        Log.AppendText(Environment.NewLine + "Update completed.")
+        Log.AppendText(Environment.NewLine + "")
         Process.Start("PS_OS.exe")
-        Threading.Thread.Sleep(1000)
+        Log.AppendText(Environment.NewLine + "Restarting...")
+        Timer5.Enabled = True
+    End Sub
+
+    Private Sub Timer5_Tick(sender As Object, e As EventArgs) Handles Timer5.Tick
+        Timer5.Enabled = False
         For Each u As Process In Process.GetProcesses
             If u.ProcessName = "Updater_PS" Then
                 u.Kill()
